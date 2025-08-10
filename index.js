fe3b7bf9befe6f3e10601b53a41290cd71282b62
LALITHA PRIYANKA MULLU <142783436+PRIYANKA-MULLU@users.noreply.github.com>
2025-08-10 13:42:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+<html lang="{i18n.language}"></html>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <App />
+
+);
+
+
